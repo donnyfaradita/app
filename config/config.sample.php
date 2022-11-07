3d66e1fd31cdad71d1fd5c51c11884998dd60949
donnyfaradita <117655072+donnyfaradita@users.noreply.github.com>
2022-11-07 20:39:58 +0700

    Create config.sample.php

---
+++ config/config.sample.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Configuration for KirimWA.id
+ */
+return [
+    // Indonesia country code is 62
+    'countryCode' => '62',
+
+    // Database connection DSN string
+    // e.g sqlite:./data/somefile.sqlite3
+    'dsnConn' => '',
+
+    // Default hostname
+    'hostname' => 'localhost:9595',
+
+    // Default protocol
+    'protocol' => 'http://',
+
+    // HTML Metadata
+    'metadata' => [
+        'title' => '',
+        'description' => '',
+        'keywords' => '',
+        'imageUrl' => '',
+        'type' => 'website'
+    ]
+];
